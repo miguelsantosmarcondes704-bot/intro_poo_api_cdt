26552b1ae519bbd96be99ab13bcd2aeebf1fdae0
progamacao-cdtl <miguelsantosmarcondes704@gmail.com>
2026-05-13 14:53:57 -0300

    Update exercicio_pratico_bd.py

--- modulo_11_MiguelMarcondes/exercicio_pratico_bd.py
+++ modulo_11_MiguelMarcondes/exercicio_pratico_bd.py
@@ -77,9 +77,79 @@ cursor.execute('''
      for cliente in clientes:
      print(cliente)
                
-               cursor. execute(''')
+
+cursor. execute(''')
+
+
+
+UPDATE clientes SET email = 'novo_email@dominio.com' WHERE id = 1
+
 
 ''')
+
+conn.commit()
+
+cursor.execute('''
+
+DELETE FROM clientes WHERE id = 2
+
+
+''')
+
+
+conn.commit
+
+cursor.execute('SELECT * FROM clientes')
+
+clientes = cursor.fetchall()
+
+print("Clientes após as alterações:")
+for cliente in clientes:
+    print(cliente)
+
+    cursor.execute('''
+
+
+CREATE TABLE IF NOT EXISTS pedidos (
+        id INTEGER PRIMARY KEY,
+        cliente_id INTEGER,
+        produto TEXT NOT NULL,
+        quantidade INTEGER NOT NULL,
+        FOREIGN KEY (cliente_id) REFERENCES clientes(id)
+    )
+
+cursor.execute('''
+
+SELECT pedidos.id, clientes.nome, pedidos.produto, pedidos.quantidade
+               FROM pedidos
+               JOIN clientes ON pedidos.cliente_id = clientes.id
+
+''')
+
+pedidos = cursor.fetchall()
+
+print.("Pedidos:")
+
+for pedido in pedidos:
+
+     print(pedido)
+
+    
+cursor.execute(''')
+
+    UPDATE pedidos SET quantidade = 5 WHERE id = 1
+                       
+                   
+                   ''')
+    
+conn.commit()
+
+
+
+'''
+
+
+'''
 
 
 
